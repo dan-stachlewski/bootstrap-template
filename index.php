7c442e2a9940b79a0548c688b06d1ba5d5166a32
dan-stachlewski <dan.stachlewski@gmail.com>
2015-04-23 23:14:00 +1000

    Website Template with bootstrap code

---
+++ index.php
@@ -0,0 +1,74 @@
+<!--
+/***
+ * WEBSITE TEMPLATE:
+ **
+ *
+ * TEMPLATE COMPILED: FRI 10 APRIL 2015
+ * DATE: 
+ * AUTHOR: DAN STACHLEWSKI
+ * CLIENT:
+ * DESCRIPTION:
+ * LANGUAGES USED:
+ * OTHER INFO:
+ * LINKS:
+ *
+ */
+ -->  
+
+<!DOCTYPE html>
+<html lang="en" />
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="Dan Stachlewski" />
+	<meta name="description" content=" " />
+	<meta name="keywords" content=" " />
+
+	<!-- BOOTSTRAP.CSS LINK -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+	<!-- PRETTIFY.CSS LINK -->
+	<link rel="stylesheet" type="text/css" href="css/prettify.css">
+	
+	<!-- WEBSITE.CSS CUSTOM LINK -->
+	<link rel="stylesheet" type="text/css" href="css/my-styles.css">
+	<title></title>
+</head>
+<body>
+
+<!-- START ADDING CODE HERE -->
+
+
+
+<!-- /END ADDING CODE HERE -->
+
+<!-- BOOTSTRAP.JS LINK -->
+<script src="js/bootstrap.min.js"></script>
+
+	<!-- JQUERY.JS LINK (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<!-- JQUERY.JS LINK (doesn't need internet connection) -->
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.7.2.min.js"></script>
+
+	<!-- PRETTIFY.JS LINK -->
+	<script type="text/javascript" src="js/prettify.js"></script>
+	<script type="text/javascript" src="js/run_prettify.js"></script>
+
+	<!-- PRETTIFY.JS FUNCTION (needs to be called for prettify.css & prettify.js to work) -->
+	<script>
+		prettyPrint();
+	</script>
+
+<!-- WEBSITE.JS CUSTOM LINK -->
+<script type="text/javascript" src="js/my-scripts.js"></script>
+
+
+</body>
+</html>
